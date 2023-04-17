c9ee3a0a49c04ab44c4b0add7217cc1682fb2017
Leonardo Moreno <Leonardo.Moreno.Ext@vision-box.com>
2023-04-17 13:02:32 +1000

    adding presentation configuraiton

--- src/api/Program.cs
+++ src/api/Program.cs
@@ -1,15 +1,15 @@
 using Serilog;
 using TaskProcessor.Core;
+using TaskProcessor.Presentation;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Host.UseSerilog((_, config) => config.ReadFrom.Configuration(builder.Configuration));
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 builder.Services.AddCoreApplication(builder.Configuration);
+builder.Services.AddPresentation(builder.Configuration);
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -22,27 +22,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseSerilogRequestLogging();
 app.UseHttpsRedirection();
-
-var group = app.MapGroup("task");
-
-group.MapGet("/", () =>
-{    
-    return new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };  
-})
-.WithName("GetTasks")
-.WithOpenApi();
-
-group.MapPost("/", () => 
-{
-    return Results.Created("GetTasks", new {id= 1});
-});
-
-group.MapDelete("/", () => 
-{
-    return Results.Created("GetTasks", new {id= 1});
-});
+app.UsePresentation();
 
 app.Run();
